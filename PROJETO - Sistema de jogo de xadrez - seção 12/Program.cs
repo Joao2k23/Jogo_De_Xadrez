aad13b1dfd4d9fc677de0a706f1c6660b3ae98ae
Joao Gabriel <joaombraga@outlook.com.br>
2023-07-19 19:33:40 -0300

    Criando a classe Posicao

--- PROJETO - Sistema de jogo de xadrez - seção 12/Program.cs
+++ PROJETO - Sistema de jogo de xadrez - seção 12/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Globalization;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p;
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + p);
+
+            Console.ReadLine();
+        }
+    }
+}
